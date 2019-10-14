0f822dee93d4b4b6ef34a851926f46a9774bd11d
Jesus Mejia <jesusmejded@gmail.com>
2019-10-14 15:08:11 -0500

    dep on v9.1.3

--- setup.m
+++ setup.m
@@ -1,7 +1,9 @@
 close all;
 clear all;
 
-url = 'https://github.com/jmejia8/bilevel-benchmark/archive/v9.1.2.zip';
+v = '9.1.3';
+url = strcat('https://github.com/jmejia8/bilevel-benchmark/archive/v', v ,'.zip');
+libpath = strcat('bilevel-benchmark-', v ,'/*');
 
 disp('Downloading library...');
 websave('lib.zip', url);
@@ -9,13 +11,16 @@ websave('lib.zip', url);
 disp('Uzipping library...');
 unzip('lib.zip','./');
 mkdir 'bilevel-benchmark';
-movefile 'bilevel-benchmark-9.1.2/*' 'bilevel-benchmark';
-rmdir('bilevel-benchmark-9.1.2', 's');
+movefile(libpath, 'bilevel-benchmark');
+rmdir(strcat('bilevel-benchmark-', v), 's');
 
 display('Compiling PMM_leader...');
 mex('CFLAGS="\$CFLAGS -std=c99 -lm"', 'PMM_leader.c');
 
 display('Compiling PMM_follower...');
 mex('CFLAGS="\$CFLAGS -std=c99 -lm"', 'PMM_follower.c');
+
+display('Compiling PMM_Psi...');
+mex('CFLAGS="\$CFLAGS -std=c99 -lm"', 'PMM_Psi.c');
 
 disp('Done!');
